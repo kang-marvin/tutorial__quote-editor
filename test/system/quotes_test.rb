3ec2b33a95e3f18b31dc91768f32a15aac9dd462
Marvin Kang'ethe <marvinjameskang@gmail.com>
2022-12-05 16:51:43 +0300

    Create quote fixtures and syste test for quote CRUD

---
+++ test/system/quotes_test.rb
@@ -0,0 +1,60 @@
+require "application_system_test_case"
+
+class QuotesTest < ApplicationSystemTestCase
+
+  setup do
+    @quote = quotes(:first)
+  end
+
+  test "visiting the index" do
+    visit quotes_url
+
+    assert_selector "h1", text: "Quotes"
+  end
+
+  test "Creating a new quote" do
+    visit quotes_path
+
+    click_on "New Quote"
+    assert_selector "h1", text: "New Quote"
+
+    fill_in "Name", with: "Capybara Quote"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara Quote"
+  end
+
+  test "Showing a quote" do
+    visit quotes_path
+
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit Quote"
+
+    fill_in "Name", with: "#{@quote.name} Updated!"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "#{@quote.name} Updated!"
+  end
+
+
+  test "Destroying a quote" do
+    visit quotes_path
+
+    assert_text @quote.name
+    click_on "Delete", match: :first
+
+    assert_no_text @quote.name
+  end
+
+
+end
