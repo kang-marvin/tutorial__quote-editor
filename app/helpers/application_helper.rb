927d8ac94eb56c766b4a912ed1a74bd9656f4400
Marvin Kang'ethe <marvinjameskang@gmail.com>
2022-12-06 15:15:35 +0300

    Use the "form_with" which is a mix of form_tag and form_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def submit_button_label(obj)
+    submit_type = obj.id ? "Update" : "Create"
+    "#{submit_type} #{model_class_name(obj)}"
+  end
+
+  private
+
+  def model_class_name(obj)
+    obj.class.name.downcase
+  end
 end
